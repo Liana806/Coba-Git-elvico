7e112286f9abdf645cb87b49bb474a44e35282a7
Elsaday Sambou <elsadayjuliana.official@gmail.com>
2024-06-27 01:12:29 +0800

    Initial Comit button

--- app/src/main/java/com/example/mytestinggit/MainActivity.java
+++ app/src/main/java/com/example/mytestinggit/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mytestinggit;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        button2=findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button clicked",Toast.LENGTH_SHORT).show();
+            }
         });
+
+//            Button button2 = findViewById(R.id.button2);
+//            button2.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Toast.makeText(MainActivity.this, "Button clicked",Toast.LENGTH_SHORT).show();
+//                    // Do something in response to button click
+//                }
+//            });
+        };
     }
-}
+
